c6054ccd9f75b73ff4b000369be66ac2073ab96e
sajal <sajal.mainhuna@gmail.com>
2025-08-08 10:47:30 +0530

    Merge remote-tracking branch 'origin/master'

--- 5.PrimeNumbersEfficient.py
+++ 5.PrimeNumbersEfficient.py
@@ -1,16 +1,15 @@
+import math
 def prime_preprocessing(n):
-    count = 0
-    true_primes = {}
+    is_prime_list = [True] * (n+1)
+    is_prime_list[0] = is_prime_list[1] = False
+
+    for i in range(2,math.ceil(n**0.5)):
+        if is_prime_list[i]:
+            for j in range(i*i,n+1,+i):
+                is_prime_list[j] = False
+
     for i in range(2,n+1):
-        true_primes[i] = True
-    for i in true_primes.keys():
-        if true_primes[i]:
-            j = i*i
-            while j in true_primes.keys():
-                if true_primes[j] is True:
-                    true_primes[j] = False
-                j+=i
-            count+=1
+        if is_prime_list[i]:
             print(i)
 
 prime_preprocessing(int(input("Enter the total number of primes you want : ")))
